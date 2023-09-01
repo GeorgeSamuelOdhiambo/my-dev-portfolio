25e8ff4e2549fd8194e27731c26025a6c059d102
George <george.samuel@sgnewtech.com>
2023-09-01 18:55:51 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,6 @@ export default function Home() {
       <Experience />
       <Projects />
       <ContactUs />
-      {/* <Proficiency /> */}
       <GithubProfileCard {...githubProfileData} />
     </div>
   );
